293752328a4a3fef9eebdc32022b481d2cda2101
Michael Lass <bevan@bi-co.net>
2019-03-23 13:46:42 +0100

    Improve help output

--- archlinux-java-run.sh
+++ archlinux-java-run.sh
@@ -26,30 +26,24 @@
 min=6
 max=20
 
+VERSION=5
 JAVADIR=###JAVADIR###
 
 function print_usage {
   cat << EOF
 
-archlinux-java-run [-a|--min MIN] [-b|--max MAX] [-p|--package PKG]
-                   [-f|--feature FEATURE] [-h|--help]
-                   -- JAVA_ARGS
+USAGE:
+  archlinux-java-run [-a|--min MIN] [-b|--max MAX] [-p|--package PKG]
+                     [-f|--feature FEATURE] [-h|--help]
+                     -- JAVA_ARGS
 
 EOF
 }
 
 function print_help {
-  print_usage
   cat << EOF
-Examples:
-  archlinux-java-run --max 8 -- -jar /path/to/application.jar
-    (launches java in version 8 or below)
 
-  archlinux-java-run --package 'jre/jre|jdk' -- -jar /path/to/application.jar
-    (launches Oracle's java from one of the jre or jdk AUR packages)
-
-  archlinux-java-run --feature 'javafx' -- -jar /path/to/application.jar
-    (launches java which contains a javafx implementation)
+archlinux-java-run, Version v$VERSION
 
 archlinux-java-run is a helper script used to launch Java applications
 that have specific demands on version or provider of the used JVM.
@@ -58,6 +52,21 @@ find a suitable version. If the user's default JVM is eligible, it will
 be used. Otherwise, if multiple eligible versions are installed, the
 newest Java generation is used. If multiple packages are available for
 this version, the one corresponding to the user's default JVM is used.
+EOF
+  print_usage
+  cat << EOF
+AVAILABLE FEATURES:
+  javafx: Test if JVM provides support for JavaFX.
+
+EXAMPLES:
+  archlinux-java-run --max 8 -- -jar /path/to/application.jar
+    (launches java in version 8 or below)
+
+  archlinux-java-run --package 'jre/jre|jdk' -- -jar /path/to/application.jar
+    (launches Oracle's java from one of the jre or jdk AUR packages)
+
+  archlinux-java-run --feature 'javafx' -- -jar /path/to/application.jar
+    (launches a JVM that supports JavaFX)
 
 EOF
 }

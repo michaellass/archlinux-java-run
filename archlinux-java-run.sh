7cf08c594701a86542cd1d808aae6e11b5f20e74
Michael Lass <bevan@bi-co.net>
2019-11-17 23:23:26 +0100

    Exec as early as possible
    Having a presorted list of candidates, we can exec as soon as we have
    found a valid JRE. No reason to do additional checks on all other JREs.

--- archlinux-java-run.sh
+++ archlinux-java-run.sh
@@ -208,126 +208,88 @@ fi
 
 candidates=$(generate_candiates)
 
-eligible=( )
 for ver in $candidates; do
-  if [[ $ver =~ $exp ]]; then
 
-    major=$(cut -d- -f2 <<< "$ver")
+  major=$(cut -d- -f2 <<< "$ver")
 
-    # Test for each of the required features
-    for ft in "${features[@]}"; do
-      case "$ft" in
-      javafx)
-        if [[ $major -lt 9 ]]; then
-          testcmd="/usr/lib/jvm/${ver}/bin/java -jar ${JAVADIR}/archlinux-java-run/TestJavaFX.jar"
-        else
-          mpath=$(eval echo "/usr/lib/jvm/{${ver},java-${major}-openjfx}/lib/{javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web,javafx-swt}.jar" | tr ' ' :)
-          testcmd="/usr/lib/jvm/${ver}/bin/java --module-path $mpath --add-modules ALL-MODULE-PATH -jar ${JAVADIR}/archlinux-java-run/TestJavaFX.jar"
-        fi
-        if [ $verbose -eq 1 ]; then
-          echo "Testing command: $testcmd"
-        fi
-        $testcmd
-        if [ $? -ne 0 ]; then
-          continue 2
-        fi
-        ;;
-      *)
-        echo "Ignoring request for unknown feature $ft"
-        ;;
-      esac
-    done
-
-    eligible+=( "$ver" )
-  fi
-done
-
-if [ "${#eligible[@]}" -eq 0 ]; then
-  echo "No suitable JVM found."
-  echo "Available:         "$available
-  echo "Min. required:     "$min
-  echo "Max. required:     "$max
-  echo "Package required:  "$package
-  echo "Features required: "${features[@]}
-  exit 1
-fi
-
-if [ $verbose -eq 1 ]; then
-  echo "Eligible JVMs: ${eligible[@]}"
-fi
-
-# If default JRE is suitable, bypass any remaining logic
-if [[ " ${eligible[@]} " =~ " $default " ]]; then
-  chosen_ver=$(cut -d- -f2 <<< "$default")
-  chosen_pkg=$default
-else
-  candidates=( )
-  chosen_ver=0
-  for ver in "${eligible[@]}"; do
-    jvm_ver=$(cut -d- -f2 <<< "$ver")
-    if [ $chosen_ver -eq 0 ]; then
-      chosen_ver=$jvm_ver
-    elif [ $chosen_ver -gt $jvm_ver ]; then
-      break
-    fi
-    candidates+=( "$ver" )
+  # Test for each of the required features
+  for ft in "${features[@]}"; do
+    case "$ft" in
+    javafx)
+      if [[ $major -lt 9 ]]; then
+        testcmd="/usr/lib/jvm/${ver}/bin/java -jar ${JAVADIR}/archlinux-java-run/TestJavaFX.jar"
+      else
+        mpath=$(eval echo "/usr/lib/jvm/{${ver},java-${major}-openjfx}/lib/{javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web,javafx-swt}.jar" | tr ' ' :)
+        testcmd="/usr/lib/jvm/${ver}/bin/java --module-path $mpath --add-modules ALL-MODULE-PATH -jar ${JAVADIR}/archlinux-java-run/TestJavaFX.jar"
+      fi
+      if [ $verbose -eq 1 ]; then
+        echo "Testing command: $testcmd"
+      fi
+      $testcmd
+      if [ $? -ne 0 ]; then
+        continue 2
+      fi
+      ;;
+    *)
+      echo "Ignoring request for unknown feature $ft"
+      ;;
+    esac
   done
 
-  pref_package=$(cut -d- -f3- <<< "$default")
-  pref_versioned="java-${chosen_ver}-${pref_package}"
+  for ft in "${features[@]}"; do
+    case "$ft" in
+    javafx)
+      if [[ $major -gt 8 ]]; then
+        echo "Modifying java arguments to support system installation of JavaFX"
+        # Extend --module-path and --add-modules to support JavaFX
+        additional_mpath=$(eval echo "/usr/lib/jvm/{${ver},java-${major}-openjfx}/lib/{javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web,javafx-swt}.jar" | tr ' ' :)
+        additional_mods=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web
+        mpath_set=0
+        mods_set=0
+        for i in "${!java_args[@]}"; do
+          case "${java_args[$i]}" in
+          --module-path=*)
+            java_args[$i]="${java_args[$i]}:${additional_mpath}"
+            mpath_set=1
+            ;;
+          --module-path)
+            java_args[$((i+1))]="${java_args[$((i+1))]}:${additional_mpath}"
+            mpath_set=1
+            ;;
+          --add-modules=*)
+            java_args[$i]="${java_args[$i]},${additional_mods}"
+            mods_set=1
+            ;;
+          --add-modules)
+            java_args[$((i+1))]="${java_args[$((i+1))]},${additional_mods}"
+            mods_set=1
+            ;;
+          esac
+        done
+        if [ $mods_set -eq 0 ]; then
+          java_args=("--add-modules=${additional_mods}" "${java_args[@]}")
+        fi
+        if [ $mpath_set -eq 0 ]; then
+          java_args=("--module-path=${additional_mpath}" "${java_args[@]}")
+        fi
+      fi
+      ;;
+    esac
+  done
 
-  if [[ " ${candidates[@]} " =~ " ${pref_versioned} " ]]; then
-    chosen_pkg=$pref_versioned
-  elif [[ " ${candidates[@]} " =~ " java-${chosen_ver}-openjdk " ]]; then
-    chosen_pkg=java-${chosen_ver}-openjdk
-  else
-    chosen_pkg=$candidates
+  if [ $verbose -eq 1 ]; then
+    echo "Executing command: /usr/lib/jvm/${ver}/bin/java ${java_args[@]}"
   fi
-fi
 
-for ft in "${features[@]}"; do
-  case "$ft" in
-  javafx)
-    if [[ $chosen_ver -gt 8 ]]; then
-      echo "Modifying java arguments to support system installation of JavaFX"
-      # Extend --module-path and --add-modules to support JavaFX
-      additional_mpath=$(eval echo "/usr/lib/jvm/{${chosen_pkg},java-${chosen_ver}-openjfx}/lib/{javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web,javafx-swt}.jar" | tr ' ' :)
-      additional_mods=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web
-      mpath_set=0
-      mods_set=0
-      for i in "${!java_args[@]}"; do
-        case "${java_args[$i]}" in
-        --module-path=*)
-          java_args[$i]="${java_args[$i]}:${additional_mpath}"
-          mpath_set=1
-          ;;
-        --module-path)
-          java_args[$((i+1))]="${java_args[$((i+1))]}:${additional_mpath}"
-          mpath_set=1
-          ;;
-        --add-modules=*)
-          java_args[$i]="${java_args[$i]},${additional_mods}"
-          mods_set=1
-          ;;
-        --add-modules)
-          java_args[$((i+1))]="${java_args[$((i+1))]},${additional_mods}"
-          mods_set=1
-          ;;
-        esac
-      done
-      if [ $mods_set -eq 0 ]; then
-        java_args=("--add-modules=${additional_mods}" "${java_args[@]}")
-      fi
-      if [ $mpath_set -eq 0 ]; then
-        java_args=("--module-path=${additional_mpath}" "${java_args[@]}")
-      fi
-    fi
-    ;;
-  esac
+  exec /usr/lib/jvm/${ver}/bin/java "${java_args[@]}"
+
 done
 
-if [ $verbose -eq 1 ]; then
-  echo "Executing command: /usr/lib/jvm/${chosen_pkg}/bin/java ${java_args[@]}"
-fi
-
-exec /usr/lib/jvm/${chosen_pkg}/bin/java "${java_args[@]}"
+echo "No suitable JVM found."
+echo "Available:         "$available
+echo "Min. required:     "$min
+echo "Max. required:     "$max
+echo "Package required:  "$package
+echo "Candidates:        "$candidates
+echo "Features required: "${features[@]}
+exit 1
